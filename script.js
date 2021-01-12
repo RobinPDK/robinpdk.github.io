fd7dce7f9e06464fcc255bed6b7755597625c558
RobinPDK <77338626+RobinPDK@users.noreply.github.com>
2021-01-12 22:06:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,34 +37,6 @@ function Submitted() {
   huts.style.display = "block";
 }
 
-"./website/components/structures/whatsapp-button/whatsapp-button.js": (__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-  "use strict";
-  __webpack_require__.r(__webpack_exports__), __webpack_require__.d(__webpack_exports__, {
-    default: () => WhatsappButtonComponent
-  });
-  __webpack_require__("./node_modules/core-js/modules/web.dom-collections.iterator.js");
-  var _assets_scripts_modules_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("./website/assets/scripts/modules/component.js");
-  class WhatsappButtonComponent extends _assets_scripts_modules_component__WEBPACK_IMPORTED_MODULE_1__.Z {
-    init() {
-      this.button = this.element.querySelector('.whatsapp-button__close'), this.timeout = null, 
-      this.button && (this.focusableElements = [ ...this.element.querySelectorAll('a[href], button, input, select') ], 
-      this.focusableElements.forEach((element => element.setAttribute('tabindex', -1))), 
-      this.button.addEventListener('click', (() => this.hideButton())), 'true' === sessionStorage.getItem('whatsapp-button') ? this.showButton() : 'false' === sessionStorage.getItem('whatsapp-button') ? this.hideButton() : (window.clearTimeout(this.timeout), 
-      this.timeout = window.setTimeout((() => this.showButton()), 3e4)));
-    }
-    showButton() {
-      this.focusableElements.forEach((element => element.removeAttribute('tabindex'))), 
-      window.requestAnimationFrame((() => this.element.classList.add('whatsapp-button--visible'))), 
-      sessionStorage.setItem('whatsapp-button', 'true');
-    }
-    hideButton() {
-      this.focusableElements.forEach((element => element.setAttribute('tabindex', -1))), 
-      window.requestAnimationFrame((() => this.element.classList.remove('whatsapp-button--visible'))), 
-      sessionStorage.setItem('whatsapp-button', 'false');
-    }
-  }
-  window.addEventListener('init-load', (() => document.querySelectorAll('.whatsapp-button').forEach((element => {
-    element.instance = element.instance || new WhatsappButtonComponent(element);
-  }
+
 
 //goedenavond
